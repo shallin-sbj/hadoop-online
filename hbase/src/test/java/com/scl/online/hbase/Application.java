c111012eebeb2cec95d8094f7f576b498d38b775
User <sucuilin27@sina.com>
2019-08-13 23:46:21 +0800

    init

--- hbase/src/test/java/com/scl/online/hbase/Application.java
+++ hbase/src/test/java/com/scl/online/hbase/Application.java
@@ -1,25 +1,16 @@
 package com.scl.online.hbase;
 
-import org.apache.hadoop.fs.BlockLocation;
-import org.apache.hadoop.fs.FSDataInputStream;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.List;
-import java.util.Map;
 
 /**
  * 测试HDFS的基本操作
  */
 @RunWith(SpringRunner.class)
-@ComponentScan("com.scl.hadoop.hbase")
+@ComponentScan("com.scl.online")
 @SpringBootTest
 public class Application {
 
